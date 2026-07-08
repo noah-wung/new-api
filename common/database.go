960ba529715699004652d1dea17091be6b457133
noah-wung <rushuiwushi@gmail.com>
2026-07-08 21:39:44 +0800

    fix: add missing UsingMainDatabase/UsingLogDatabase helpers
    These functions and DatabaseTypeClickHouse are referenced by merged
    commits (70ea899e3 row locking, a162163b4 task cleanup, dfc0d6324
    security) but defined in 6dc4030fd (ClickHouse log DB), which was
    skipped. Add the DatabaseType type, ClickHouse const, the
    main/logDatabaseType vars, Using*/Set* helpers, and wire Set* calls
    into model/main.go DB init.

--- common/database.go
+++ common/database.go
@@ -1,9 +1,12 @@
 package common
 
+type DatabaseType string
+
 const (
-	DatabaseTypeMySQL      = "mysql"
-	DatabaseTypeSQLite     = "sqlite"
-	DatabaseTypePostgreSQL = "postgres"
+	DatabaseTypeMySQL      DatabaseType = "mysql"
+	DatabaseTypeSQLite     DatabaseType = "sqlite"
+	DatabaseTypePostgreSQL DatabaseType = "postgres"
+	DatabaseTypeClickHouse DatabaseType = "clickhouse"
 )
 
 var UsingSQLite = false
@@ -11,5 +14,24 @@ var UsingPostgreSQL = false
 var LogSqlType = DatabaseTypeSQLite // Default to SQLite for logging SQL queries
 var UsingMySQL = false
 var UsingClickHouse = false
+
+var mainDatabaseType DatabaseType = DatabaseTypeSQLite
+var logDatabaseType DatabaseType = DatabaseTypeSQLite
+
+func UsingMainDatabase(databaseType DatabaseType) bool {
+	return mainDatabaseType == databaseType
+}
+
+func UsingLogDatabase(databaseType DatabaseType) bool {
+	return logDatabaseType == databaseType
+}
+
+func SetMainDatabaseType(databaseType DatabaseType) {
+	mainDatabaseType = databaseType
+}
+
+func SetLogDatabaseType(databaseType DatabaseType) {
+	logDatabaseType = databaseType
+}
 
 var SQLitePath = "one-api.db?_busy_timeout=30000"
--- model/main.go
+++ model/main.go
@@ -126,8 +126,10 @@ func chooseDB(envName string, isLog bool) (*gorm.DB, error) {
 			common.SysLog("using PostgreSQL as database")
 			if !isLog {
 				common.UsingPostgreSQL = true
+				common.SetMainDatabaseType(common.DatabaseTypePostgreSQL)
 			} else {
 				common.LogSqlType = common.DatabaseTypePostgreSQL
+				common.SetLogDatabaseType(common.DatabaseTypePostgreSQL)
 			}
 			return gorm.Open(postgres.New(postgres.Config{
 				DSN:                  dsn,
@@ -140,8 +142,10 @@ func chooseDB(envName string, isLog bool) (*gorm.DB, error) {
 			common.SysLog("SQL_DSN not set, using SQLite as database")
 			if !isLog {
 				common.UsingSQLite = true
+				common.SetMainDatabaseType(common.DatabaseTypeSQLite)
 			} else {
 				common.LogSqlType = common.DatabaseTypeSQLite
+				common.SetLogDatabaseType(common.DatabaseTypeSQLite)
 			}
 			return gorm.Open(sqlite.Open(common.SQLitePath), &gorm.Config{
 				PrepareStmt: true, // precompile SQL
@@ -159,8 +163,10 @@ func chooseDB(envName string, isLog bool) (*gorm.DB, error) {
 		}
 		if !isLog {
 			common.UsingMySQL = true
+			common.SetMainDatabaseType(common.DatabaseTypeMySQL)
 		} else {
 			common.LogSqlType = common.DatabaseTypeMySQL
+			common.SetLogDatabaseType(common.DatabaseTypeMySQL)
 		}
 		return gorm.Open(mysql.Open(dsn), &gorm.Config{
 			PrepareStmt: true, // precompile SQL
@@ -169,6 +175,7 @@ func chooseDB(envName string, isLog bool) (*gorm.DB, error) {
 	// Use SQLite
 	common.SysLog("SQL_DSN not set, using SQLite as database")
 	common.UsingSQLite = true
+	common.SetMainDatabaseType(common.DatabaseTypeSQLite)
 	return gorm.Open(sqlite.Open(common.SQLitePath), &gorm.Config{
 		PrepareStmt: true, // precompile SQL
 	})
